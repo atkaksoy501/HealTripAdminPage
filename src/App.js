8e1e16fa82d2bd0c7b2f2599c8199841a8282218
boerten <boerten@hotech.club>
2024-05-04 20:34:32 +0300

    update project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import LoginPage from './login/LoginPage';
+import { LoginPage } from "./login/LoginPage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './Home'; 
 
